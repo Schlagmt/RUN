6dd46c2f63e9e95608dc7a0178045128eab5c787
schlagmt <mtschlager@gmail.com>
2022-04-27 10:21:58 -0400

    transfer to clustering... needs work

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import run
 import plot 
 import os
 from os import listdir
+from sklearn.cluster import OPTICS, cluster_optics_dbscan
+from geopy.geocoders import Nominatim
 
 def readin_data(file):
     route = run.run(file)
@@ -18,69 +20,60 @@ def readin_data(file):
 
 def remove_outlier_data(data):
     
-    x_list_mean = []
-    y_list_mean = []
-    x_list = []
-    y_list = []
-    for d in data:
-        x_list.extend(d.x)
-        y_list.extend(d.y)
-        x_list_mean.append(np.mean(d.x))
-        y_list_mean.append(np.mean(d.y))
+    xy_coordinates = [[d.x[0],d.y[0]] for d in data]
+    X = np.vstack(np.array(xy_coordinates))
+    clust = OPTICS(min_samples=5, xi=0.01, min_cluster_size=0.05)
 
-    x_mean = np.mean(x_list_mean)
-    y_mean = np.mean(y_list_mean)
+    # Run the fit
+    clust.fit(X)
 
-    x_standard_deviation = np.std(x_list)
-    y_standard_deviation = np.std(y_list)
-
-    data_without_outlier = []
+    groupings = [[] for x in range(min(clust.labels_), max(clust.labels_))]
     for index, d in enumerate(data):
-        if not ((x_list_mean[index] > x_mean + 2*x_standard_deviation or x_list_mean[index] < x_mean - 2*x_standard_deviation) or (y_list_mean[index] > y_mean + 2*y_standard_deviation or y_list_mean[index] < y_mean - 2*y_standard_deviation)):
-            data_without_outlier.append(d)
+        groupings[clust.labels_[index]].append(d)
+    
+    return groupings
 
-    return data_without_outlier
+def get_approximate_locations(data):
+    locations = []
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    for d in data:
+        loc = geolocator.reverse(str(d[1]) + ',' + str(d[0]))
+        address = loc.raw['address']
+        locations.append(address.get('county',''))
+
+    return locations
+
 
 def main():
 
     pool = multiprocessing.Pool(4)
     runs = pool.map(readin_data, listdir('RawData/'))
 
-    type_names = set(([r.name for r in runs]))
-    type_values = []
-    temp_data = runs
-    for type_name in type_names:
-        temp_count = 0
-        for index, r in enumerate(temp_data):
-            if (r.name == type_name):
-                temp_count += 1
+    clustered_data = remove_outlier_data(runs)
 
-        type_values.append({
-            'type_name': type_name,
-            'count': temp_count
-        })
-    type_values.sort(key = lambda x : x['count'], reverse=True)
-    for index, type_value in enumerate(type_values):
-        print(str(index+1) + '\t->\t'  + 'Count: '+ str(type_value['count']) + '\t| ' + type_value['type_name'])
+    approximate_locations = get_approximate_locations([[c[0].x[0], c[0].y[0]] for c in clustered_data])
+
+    for index, cluster in enumerate(clustered_data):
+        print(str(index+1) + '\t->\t'  + 'Count: '+ str(len(cluster)) + '\t| ' + approximate_locations[index])
 
     quit_loop = True
     while(quit_loop):
         input_index = input("Enter the value you wanted plotted: ")
-        values_to_be_plotted = [r for r in runs if r.name == (type_values[int(input_index)-1])['type_name']]
+        values_to_be_plotted = clustered_data[int(input_index) - 1]
         input_points_per_frame = input("Enter points per frame (Runs: 5, Walks: 10): ")
 
-        values_to_be_plotted_minus_outlier = remove_outlier_data(values_to_be_plotted)
-        plot_outlier = 'Y'
-        if (len(values_to_be_plotted) - len(values_to_be_plotted_minus_outlier) > 0):
-            print('There are ' + str(len(values_to_be_plotted) - len(values_to_be_plotted_minus_outlier)) + ' courses considered outlier')
-            plot_outlier = input('Include outlier? (Y or N): ')
+        #values_to_be_plotted_minus_outlier = remove_outlier_data(values_to_be_plotted)
+        #plot_outlier = 'Y'
+        #if (len(values_to_be_plotted) - len(values_to_be_plotted_minus_outlier) > 0):
+            #print('There are ' + str(len(values_to_be_plotted) - len(values_to_be_plotted_minus_outlier)) + ' courses considered outlier')
+            #plot_outlier = input('Include outlier? (Y or N): ')
 
-        if (plot_outlier == 'Y'):
-            new_plot = plot.plot(values_to_be_plotted)
-            new_plot.build_plot((type_values[int(input_index)-1])['type_name'], int(input_points_per_frame))
-        else:
-            new_plot = plot.plot(values_to_be_plotted_minus_outlier)
-            new_plot.build_plot((type_values[int(input_index)-1])['type_name'], int(input_points_per_frame))
+        #if (plot_outlier == 'Y'):
+        #    new_plot = plot.plot(values_to_be_plotted)
+        #    new_plot.build_plot((type_values[int(input_index)-1])['type_name'], int(input_points_per_frame))
+        #else:
+        new_plot = plot.plot(values_to_be_plotted)
+        new_plot.build_plot(approximate_locations[int(input_index)-1], int(input_points_per_frame))
 
         input_quit = input("Quit? (Y or N): ")
         if input_quit == 'Y':
